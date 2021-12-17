d79ef3c89d01fc6365e02351ff2d35b20c041d06
Khalil Howard <khalil.howard13@hotmail.com>
2021-12-17 05:21:30 -0600

    prep for deployment

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
-
 //reference elements
 const slot1 = document.querySelector('#slot1')
 const slot2 = document.querySelector('#slot2')
 const slot3 = document.querySelector('#slot3')
 const slotChoices = ["1", "2", "3"]
+const resultEl = document.querySelector('#message')
 
 //event listener
 document.querySelector("button").addEventListener('click', slotSpin)
@@ -12,10 +12,8 @@ document.querySelector("button").addEventListener('click', slotSpin)
 function randChoice() {
     let number=Math.floor(Math.random()*slotChoices.length);
     console.log(number)
-    return slotChoices[number];
+    return slotChoices[number]; 
 }
-
-//initial game
 
 //Play game
 function slotSpin() {
@@ -30,19 +28,20 @@ function slotSpin() {
     if(option1 === option2 && option1 === option3) {
         youWin();
     } else {
-        hideMessage();
+        youLose();
     }
 }
 
 function youWin() {
     const msg = document.querySelector('#message');
     msg.style.display = "block";
+    resultEl.innerHTML = "Winner, Winner";
 }
 
-function hideMessage() {
+function youLose() {
     const msg = document.querySelector('#message')
-    msg.style.display = "none";
-    return `Try Again`
+    msg.style.display = "block";
+    resultEl.innerHTML = "Try Again!";
 }
 
 slotSpin()
